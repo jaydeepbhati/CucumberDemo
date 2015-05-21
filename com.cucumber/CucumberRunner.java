e16753bcd3013aaf8b3f2c694d22f8324ee1be0f
jaydeepsingh <jaydeep.bhati@gmail.com>
2015-05-21 18:11:48 +0530

    create

---
+++ com.cucumber/CucumberRunner.java
@@ -0,0 +1,16 @@
+package com.cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = {"pretty", "json:Target/"} ,
+		features = "Features"
+		)
+
+public class CucumberRunner {
+	
+}
